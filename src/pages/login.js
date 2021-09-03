b863351c6fa686679127e1109860a5318eeee5ec
Abishek002-P <abishekprabakaran2003@gmail.com>
2021-09-03 19:21:47 +0530

    Updated scss

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 
 
+
+
 export default function login() {
     return (
         <div>
-          <h1>Abishek P</h1>
+          <h1 className="landing-text">Abishek P</h1>
         </div>
     )
 }
