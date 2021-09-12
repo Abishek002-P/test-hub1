088e5dc0035fc9bf70622645133242b954b97801
Abishek002-P <abishekprabakaran2003@gmail.com>
2021-09-12 11:51:20 +0530

    Edited text

--- src/pages/login.js
+++ src/pages/login.js
@@ -8,6 +8,9 @@ export default function login() {
     return (
         <div>
           <h1 className="landing-text">Abishek P</h1>
+          <div class="btn-text-center">
+  	<button type="button" class="btn btn-primary">Center Align Button</button>
+</div>
         </div>
     )
 }
