6a62e8573cf06c6bb27fbaf2a917b6baef9e66cf
Abishek002-P <abishekprabakaran2003@gmail.com>
2021-09-22 22:48:52 +0530

    Fully functional login system

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,16 +1,40 @@
 import React from 'react'
 
+const Login = (props) => {
+
+       
+       const {email, setEmail, password, setPassword, handleLogin, handleSignup, hasAccount, setHasAccount, emailError, passwordError } = props;
 
 
-
-
-export default function login() {
     return (
-        <div>
-          <h1 className="landing-text">Abishek P</h1>
-          <div class="btn-text-center">
-  	<button type="button" class="btn btn-primary">Center Align Button</button>
-</div>
-        </div>
+        <section className="login">
+           <div className="loginContainer">
+               <label> Username </label>
+               <input type="text" autoFocus required value={email} onChange={(e) => setEmail(e.target.value)}  />
+               <p className="errorMsg">{emailError}</p>
+               <label>Password</label>
+               <input type="password" required value={password} onChange={(e) => setPassword(e.target.value)} />
+               <p className="errorMsg">{passwordError}</p>
+               <div className="btnContainer">
+                   {hasAccount ? (
+                       <>
+                        <button onClick={handleLogin}>Sign in</button>
+                        <p>Don't have an account ? 
+                            <span onClick={() => setHasAccount(!hasAccount)}>Sign Up</span>
+                        </p>
+                       </>
+                   ) : (
+                       <>
+                        <button onClick={handleSignup}>Sign Up</button>
+                        <p>Have an account ? 
+                            <span onClick={() => setHasAccount(!hasAccount)}>Sign in</span>
+                        </p>
+                       </>
+                   )}
+               </div>
+           </div>
+        </section>
     )
 }
+
+export default Login;
